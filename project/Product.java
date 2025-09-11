eb255cd92092d22fe27977c05b179357123d9ea4
sahhhad <sahadb007@gmail.com>
2025-09-11 17:51:44 +0530

    Create Product.java

---
+++ project/Product.java
@@ -0,0 +1,13 @@
+public class Product {
+    int id;
+    String name;
+    int quantity;
+    double price;
+
+    public Product(int id, String name, int quantity, double price) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+}
