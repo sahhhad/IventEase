b3f37a88fdcf5643571ee7fdc8e064ee817429f0
ASWATHY SHYJI <aswathyshyji22@gmail.com>
2025-10-13 18:32:40 +0530

    Remove empty project folders

--- README.md
+++
@@ -1,5 +0,0 @@
-
-# IventEase
-
-# inventory
-02d053c304c9a3afa81a282d658238b29967d41e
--- project/User.java
+++
@@ -1,9 +0,0 @@
-public class User {
-    String username;
-    String password;
-
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-}
